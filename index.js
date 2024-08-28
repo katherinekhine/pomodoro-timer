ad550350a3d8df45bb153efe38052959e8314b6b
katherinekhine <khaingkatherine@gmail.com>
2024-08-28 18:42:30 +0630

    update index.js

--- index.js
+++ index.js
@@ -9,12 +9,35 @@ let timeLeft = 1500;
 function updateTimer() {
   let minutes = Math.floor(timeLeft / 60);
   let seconds = timeLeft % 60;
-  let formatted = `${minutes.toString().padStart(2, "0")} : ${seconds
+  let formatted = `${minutes.toString().padStart(2, "0")}:${seconds
     .toString()
     .padStart(2, "0")}`;
   timerEl.textContent = formatted;
 }
 
 function startTimer() {
-  interval = setInterval(() => {}, 1000);
+  interval = setInterval(() => {
+    timeLeft--;
+    updateTimer();
+    if (timeLeft === 0) {
+      clearInterval(interval);
+      alert("Time's up!");
+      timeLeft = 1500;
+      updateTimer();
+    }
+  }, 1000);
 }
+
+function stopTimer() {
+  clearInterval(interval);
+}
+
+function resetTimer() {
+  clearInterval(interval);
+  timeLeft = 1500;
+  updateTimer();
+}
+
+startEl.addEventListener("click", startTimer);
+stopEl.addEventListener("click", stopTimer);
+resetEl.addEventListener("click", resetTimer);
